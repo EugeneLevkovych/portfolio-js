23582a1fcbc1c6c6badc0dc5e4fad56420ff253e
EugeneLevkovych <eugenelevkovych@yahoo.com>
2025-03-09 17:06:05 +0200

    add swiper mob

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -8,3 +8,37 @@ document.addEventListener('DOMContentLoaded', function () {
     openOnInit: [0],
   });
 });
+// --------------Swiper------------------
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
+
+const swiper = new Swiper('.swiper', {
+  direction: 'horizontal',
+  spaceBetween: 0,
+  loop: true,
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+  mousewheel: {
+    enabled: true,
+    invert: true,
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 2,
+    },
+
+    768: {
+      slidesPerView: 3,
+    },
+
+    1280: {
+      slidesPerView: 6,
+    },
+  },
+  navigation: {
+    // enabled: false,
+    nextEl: '.a-m-swiper-button',
+  },
+});
